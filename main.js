0dadebbb71a49ffbdad8ed7ea2c89b39fdb41412
Estiiven <122229140+estiivenCa@users.noreply.github.com>
2024-08-17 22:33:37 -0500

    ✰ 𝙺𝚊𝚗𝙱𝚘𝚝 ✰

--- main.js
+++ main.js
@@ -288,7 +288,7 @@ if (opcion == '1') {
 console.log(chalk.yellow('⚠️ㅤEscanea este codigo QR, el codigo QR expira en 60 segundos.'))
  }}
 if (connection == 'open') {
-console.log(chalk.yellowBright('\n╭───────────────────────────◉\n│\n│Conectado correctamente al WhatsApp.\n│\n╰───────────────────────────◉\n'))}
+console.log(chalk.greenBright('\n╭───────────────────────────◉\n│\n│Conectado correctamente al WhatsApp Crack😎.\n│\n╰───────────────────────────◉\n'))}
 //if (conn.user.connect) {
 //conn.fakeReply('5217294888993@s.whatsapp.net', '😃', '0@s.whatsapp.net', '😅 Soy CuriosityBot\nRecientemente me e conectado', '0@s.whatsapp.net')
 //conn.user.connect = true;
