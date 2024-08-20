4b28f6737ceabb8a6a3b287236edbcd0323b3dea
Estiiven <122229140+estiivenCa@users.noreply.github.com>
2024-08-20 16:27:29 -0500

    Update descargas-yta.js

--- plugins/descargas-yta.js
+++ plugins/descargas-yta.js
@@ -62,6 +62,7 @@ var handler = async (m, { text, conn, args, usedPrefix, command }) => {
 
 handler.help = ['yta'];
 handler.tags = ['descargas'];
-handler.command = /^audio|fgmp3|dlmp3|mp3|getaud|yt(a|mp3|mp3)$/i;
+handler.command = /^audio|fgmp3|dlmp3|mp3|getaud|yta$/i;
+
 
 export default handler;
