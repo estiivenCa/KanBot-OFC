c16d15acc68e2c586dab62744e96d9e1ea74db87
Estiiven <122229140+estiivenCa@users.noreply.github.com>
2024-09-01 14:39:04 -0500

    Update descargas-facebook.js

--- plugins/descargas-facebook.js
+++ plugins/descargas-facebook.js
@@ -8,7 +8,7 @@ try {
 conn.reply(m.chat, `🕒 *Descargando su video de facebook.*`, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
 title: '♡  ͜ ۬︵࣪᷼⏜݊᷼𝘿𝙚𝙨𝙘𝙖𝙧𝙜𝙖𝙨⏜࣪᷼︵۬ ͜ ',
-body: '<(✿◠‿◠)> 𝙈𝙚𝙜𝙪𝙢𝙞𝙣🔥',
+body: '✰ 𝙺𝚊𝚗𝙱𝚘𝚝 ✰',
 previewType: 0, thumbnail: logo8,
 sourceUrl: channel }}})
 await m.react(rwait)
