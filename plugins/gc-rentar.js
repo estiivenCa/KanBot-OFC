0cde61cb44672434748ce7c5cce1e86e64de8001
Estiiven <122229140+estiivenCa@users.noreply.github.com>
2024-09-02 10:47:04 -0500

    Update gc-rentar.js

--- plugins/gc-rentar.js
+++ plugins/gc-rentar.js
@@ -22,7 +22,7 @@ const handler = async (m, {conn, command, args}) => {
     global.db.data.userRents = userRents;
 
     conn.reply(m.chat, `
-┌─『 𝑅𝑒𝑛𝑡𝑎𝑟 𝑎 𝑀𝑒𝑔𝑢𝑚𝑖𝑛 』*
+┌─『 𝑅𝑒𝑛𝑡𝑎𝑟 𝑎 𝙺𝚊𝚗𝙱𝚘𝚝 』*
 │╭──────────────┄
 ││ *Compra Nominal* : + ${count} Token
 ││ *Gastado* : -${xpperestrellas * count} Estrellas 🌟
