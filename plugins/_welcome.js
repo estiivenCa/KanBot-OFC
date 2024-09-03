bc4f8428e2947101acdbb823c186f60c33713e80
Estiiven <122229140+estiivenCa@users.noreply.github.com>
2024-09-03 13:07:26 -0500

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -1,5 +1,4 @@
 import { WAMessageStubType } from '@whiskeysockets/baileys';
-import {WAMessageStubType} from '@whiskeysockets/baileys';
 import fetch from 'node-fetch';
 
 export async function before(m, { conn, participants, groupMetadata }) {
