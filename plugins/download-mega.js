842cf9217fbc7bf7fcc3ef3e0975d40e52eefb45
Estiiven <122229140+estiivenCa@users.noreply.github.com>
2024-09-03 16:04:04 -0500

    Update download-mega.js

--- plugins/download-mega.js
+++ plugins/download-mega.js
@@ -1,6 +1,3 @@
-import { File } from "megajs";
-import path from "path";
-
 let handler = async (m, { conn, args, usedPrefix, text, command }) => {
     try {
         if (!text) return m.reply(`${usedPrefix + command} https://mega.nz/file/ovJTHaQZ#yAbkrvQgykcH_NDKQ8eIc0zvsN7jonBbHZ_HTQL6lZ8`, m, rcanal);
@@ -31,24 +28,11 @@ let handler = async (m, { conn, args, usedPrefix, text, command }) => {
 
         let mimetype = mimeTypes[fileExtension] || "application/octet-stream";
 
-        await conn.sendFile(m.chat, data, file.name, caption, m, null, { mimetype, asDocument: true });
+        // Verificar que m.chat es un string
+        const chatId = String(m.chat);
+        await conn.sendFile(chatId, data, file.name, caption, m, null, { mimetype, asDocument: true });
 
     } catch (error) {
         return m.reply(`Error: ${error.message}`);
     }
-}
-
-handler.help = ["mega"]
-handler.tags = ["downloader"]
-handler.command = /^(mega)$/i
-export default handler
-
-function formatBytes(bytes) {
-    if (bytes === 0) return '0 Bytes';
-
-    const k = 1024;
-    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-    const i = Math.floor(Math.log(bytes) / Math.log(k));
-
-    return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
 }
