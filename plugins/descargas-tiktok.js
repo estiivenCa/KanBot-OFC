7332a8c2e603db48df13ee8dd08dfe3a175efb3d
Estiiven <122229140+estiivenCa@users.noreply.github.com>
2024-09-07 13:46:19 -0500

    Update descargas-tiktok.js

--- plugins/descargas-tiktok.js
+++ plugins/descargas-tiktok.js
@@ -17,7 +17,7 @@ m.react('🕒')
     const prep = generateWAMessageFromContent(m.chat, {extendedTextMessage: {text: texto, contextInfo: {externalAdReply: {title: packname, body: wm, sourceUrl: yt}, mentionedJid: [m.sender]}}}, aa);
     await conn.relayMessage(m.chat, prep.message, {messageId: prep.key.id, mentions: [m.sender]});
     const dataFn = await conn.getFile(`${CFROSAPI}/api/tiktokv2?url=${args[0]}`);
-    const desc1n = `_💌  ᩭ✎Tiktok sin marca de agua descargado con éxito_`;
+    const desc1n = `_💌  ᩭ✎Tiktok sin marca de agua descargado con éxito by ✰ 𝙺𝚊𝚗𝙱𝚘𝚝 ✰_`;
     await conn.sendMessage(m.chat, {video: dataFn.data, caption: desc1n}, {quoted: fkontak});
   } catch (ee1) {
   try {
@@ -26,26 +26,26 @@ m.react('🕒')
     //await conn.relayMessage(m.chat, prep.message, {messageId: prep.key.id, mentions: [m.sender]});
     const dataF = await tiktok.v1(args[0]);
     // let desc1 =  `*𝙽𝙸𝙲𝙺𝙽𝙰𝙼𝙴:* ${dataF.nickname || 'Indefinido'}`
-    const desc1 = `_💌  ᩭ✎Tiktok sin marca de agua descargado con éxito_`;
+    const desc1 = `_💌  ᩭ✎Tiktok sin marca de agua descargado con éxito by ✰ 𝙺𝚊𝚗𝙱𝚘𝚝 ✰_`;
     await conn.sendMessage(m.chat, {video: {url: dataF.play}, caption: desc1}, {quoted: fkontak});
   } catch (e1) {
     try {
       const tTiktok = await tiktokdlF(args[0]);
       // let desc2 = `🔗 *Url:* ${tTiktok.video}`
-      const desc2 = `_💌  ᩭ✎Tiktok sin marca de agua descargado con éxito_`;
+      const desc2 = `_💌  ᩭ✎Tiktok sin marca de agua descargado con éxito by ✰ 𝙺𝚊𝚗𝙱𝚘𝚝 ✰_`;
       await conn.sendMessage(m.chat, {video: {url: tTiktok.video}, caption: desc2}, {quoted: fkontak});
     } catch (e2) {
       try {
         const p = await fg.tiktok(args[0]);
         // let te = `*𝚄𝚂𝙴𝚁𝙽𝙰𝙼𝙴:* ${p.author || 'Indefinido'}`
-        const te = `_💌  ᩭ✎Tiktok sin marca de agua descargado con éxito_`;
+        const te = `_💌  ᩭ✎Tiktok sin marca de agua descargado con éxito by ✰ 𝙺𝚊𝚗𝙱𝚘𝚝 ✰_`;
         await conn.sendMessage(m.chat, {video: {url: p.nowm}, caption: te}, {quoted: fkontak});
       } catch (e3) {
         try {
           const {author: {nickname}, video, description} = await tiktokdl(args[0]);
           const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd;
           // let cap = `*𝙽𝙸𝙲𝙺𝙽𝙰𝙼𝙴:* ${nickname || 'Indefinido'}`
-          const cap = `_💌  ᩭ✎Tiktok sin marca de agua descargado con éxito_`;
+          const cap = `_💌  ᩭ✎Tiktok sin marca de agua descargado con éxito by ✰ 𝙺𝚊𝚗𝙱𝚘𝚝 ✰_`;
           await conn.sendMessage(m.chat, {video: {url: url}, caption: cap}, {quoted: fkontak});
         } catch {
           throw `*🌟 Ocurrió un error. Por favor, inténtalo de nuevo más tarde.*`;
