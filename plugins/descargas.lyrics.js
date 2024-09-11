f63cf7d4d4493c222b49affbd6e88051e43b2e7d
Estiiven <122229140+estiivenCa@users.noreply.github.com>
2024-09-11 08:45:06 -0500

    Update descargas.lyrics.js

--- plugins/descargas.lyrics.js
+++ plugins/descargas.lyrics.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch';
+/* import fetch from 'node-fetch';
 
 // Token de acceso a la API de Genius
 const accessToken = 'y8zKszFh7WflT33NaOeGnGlOrfViwrymiwsYD-6T4D54Fk3n7hUIjYoBjZUhPJo1';
@@ -82,4 +82,50 @@ const handler = async (m, { text, command, conn }) => {
 // Define el comando que activará el handler
 handler.command = /^letra$/i;
 
+export default handler; */
+// Importaciones en ES Module
+import fetch from 'node-fetch';
+
+const handler = async (m, { conn, text, usedPrefix, command }) => {
+    if (!text) {
+        return conn.reply(m.chat, `> *\`${usedPrefix + command}\` Ingresa el texto de lo que quieres buscar*\n\n> *Ejemplo :*\n> _${usedPrefix + command} Siempre Lo Mismo - Ysy A_`, m);
+    }
+
+    try {
+        await conn.reply(m.chat, global.cargando, m);
+
+        // Llamada a la API para obtener la letra de la canción
+        let response = await fetch(`https://deliriusapi-official.vercel.app/search/letra?query=${encodeURIComponent(text)}`);
+        let ApiData = await response.json();
+
+        // Desestructurando los datos de la respuesta
+        let { title, fullTitle, artist, artistUrl, image, lyrics } = ApiData.data;
+
+        // Formateo del mensaje
+        let txt = ' *\`【 Lʏʀɪᴄꜱ Sᴇᴀʀᴄʜ 】\`*\n\n';
+        txt += `> *❀ Título:* _${title || 'x'}_\n`;
+        txt += `> *✽ Título Completo:* _${fullTitle || 'x'}_\n`;
+        txt += `> *❀ Artista:* _${artist || 'x'}_\n`;
+        txt += `> *✽ URL del Artista:* _${artistUrl || 'x'}_\n`;
+        txt += `> *ꕤ Letra:* _${lyrics || 'x'}_\n`.trim();
+
+        // Si hay una imagen, envíala con el texto, de lo contrario solo envía el texto
+        if (image) {
+            await conn.sendFile(m.chat, image, 'image.jpg', txt, m);
+        } else {
+            await conn.sendMessage(m.chat, { text: txt }, { quoted: m });
+        }
+
+        await conn.reply(m.chat, global.listo, m);
+    } catch (error) {
+        console.error(error);
+        await conn.reply(m.chat, global.error, m);
+    }
+};
+
+// Define el comando que activará el handler
+handler.command = /^letra$/i;
+
+// Exporta el handler
 export default handler;
+
