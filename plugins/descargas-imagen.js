f3d1845888f07c6efd5b8af8caf575c9994d0d09
Estiiven <122229140+estiivenCa@users.noreply.github.com>
2024-09-12 08:16:52 -0500

    Update descargas-imagen.js

--- plugins/descargas-imagen.js
+++ plugins/descargas-imagen.js
@@ -4,7 +4,7 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
 let foundProhibitedWord = prohibited.find(word => m.text.toLowerCase().includes(word));
 if (foundProhibitedWord) return conn.reply(m.chat, `🚩 *No daré resultado a tu solicitud por pajin* - Palabra prohibida: ${foundProhibitedWord}`, m);
   
-  if (!text) throw `*🚩 Uso Correcto: ${usedPrefix + command} Ai Yaemori*`;
+  if (!text) throw `*🚩 Uso Correcto: ${usedPrefix + command} Bart Simpson*`;
   conn.reply(m.chat, '🚩 *Descargando su imagen...*', m, {
   contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
   title: packname,
