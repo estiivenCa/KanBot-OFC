8d4448522d069c780039f28af7c4c463038aec0d
Estiiven <122229140+estiivenCa@users.noreply.github.com>
2024-09-13 09:52:50 -0500

    Update convertidor-togifaud.js

--- plugins/convertidor-togifaud.js
+++ plugins/convertidor-togifaud.js
@@ -4,7 +4,7 @@ conn.reply(m.chat, global.wait, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
 title: packname,
 body: wm,
-previewType: 0, thumbnail: icons,
+previewType: 0, 
 sourceUrl: channel }}})
 const q = m.quoted || m
 let mime = (q.msg || q).mimetype || ''
