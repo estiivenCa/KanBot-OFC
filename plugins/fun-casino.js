43dc4db85863d3afa88360fd144749b1eb34cd2a
Estiiven <122229140+estiivenCa@users.noreply.github.com>
2024-09-18 10:18:19 -0500

    Update fun-casino.js

--- plugins/fun-casino.js
+++ plugins/fun-casino.js
@@ -25,13 +25,13 @@ if (args.length < 1) return conn.reply(m.chat, '💣 Ingresa la cantidad de ' + 
 if (user.estrellas >= count * 1) {
 user.estrellas -= count * 1
 if (Aku > Kamu) {
-conn.reply(m.chat, '`💣 Veamos que numeros tienen!`\n\n'+ `➠ *MeguminBot* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *PERDISTE* ${formatNumber(count)} 🌟 Estrellas.`.trim(), m, rcanal)
+conn.reply(m.chat, '`💣 Veamos que numeros tienen!`\n\n'+ `➠ *KanBot* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *PERDISTE* ${formatNumber(count)} 🌟 Estrellas.`.trim(), m, rcanal)
 } else if (Aku < Kamu) {
 user.estrellas += count * 2
-conn.reply(m.chat, '`💥 Veamos que numeros tienen!`\n\n'+ `➠ *MeguminBot* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *GANASTE* ${formatNumber(count * 2)} 🌟 Estrellas.`.trim(), m, rcanal)
+conn.reply(m.chat, '`💥 Veamos que numeros tienen!`\n\n'+ `➠ *KanBot* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *GANASTE* ${formatNumber(count * 2)} 🌟 Estrellas.`.trim(), m, rcanal)
 } else {
 user.estrellas += count * 1
-conn.reply(m.chat, '`💥 Veamos que numeros tienen!`\n\n'+ `➠ *MeguminBot* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username} obtienes ${formatNumber(count * 1)} 🌟 Estrellas.`.trim(), m, rcanal)}
+conn.reply(m.chat, '`💥 Veamos que numeros tienen!`\n\n'+ `➠ *KanBot* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username} obtienes ${formatNumber(count * 1)} 🌟 Estrellas.`.trim(), m, rcanal)}
 } else conn.reply(m.chat, `No tienes *${formatNumber(count)} 🌟 Estrellas* para apostar!`.trim(), m, rcanal)}
 
 handler.help = ['apostar *<cantidad>*']
