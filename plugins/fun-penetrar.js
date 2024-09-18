87680025649d176fa36a7d4a3a9cfd0d77323abd
Estiiven <122229140+estiivenCa@users.noreply.github.com>
2024-09-18 10:20:46 -0500

    Update fun-penetrar.js

--- plugins/fun-penetrar.js
+++ plugins/fun-penetrar.js
@@ -1,4 +1,4 @@
-let handler = async (m, { conn, command, text }) => {
+/* let handler = async (m, { conn, command, text }) => {
 if (!text) throw `*Ingrese el @ o el nombre de la persona que quieras saber si te puedes ${command.replace('how', '')}*`
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 conn.reply(m.chat, `
@@ -15,4 +15,4 @@ handler.command = ['penetrar','penetrado']
 handler.register = true;
 handler.group = true;
 handler.fail = null
-export default handler
+export default handler */
