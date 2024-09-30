0c90b3e7599bb0c00b88c97bfa095e59311f6390
Estiiven <122229140+estiivenCa@users.noreply.github.com>
2024-09-30 13:10:23 -0500

    Update mods-banchat.js

--- plugins/mods-banchat.js
+++ plugins/mods-banchat.js
@@ -7,7 +7,6 @@ conn.reply(m.chat, `✅ *KanBot Ha Sido Desactivado En Este Chat*`, m, rcanal)
 handler.help = ['banchat']
 handler.tags = ['mods']
 handler.command = ['banchat']
-handler.rowner = true
-handler.owner = true 
-handler.mods = true 
+handler.mods = true;
+handler.group = true;
 export default handler
