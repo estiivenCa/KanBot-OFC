ccdf73830d17df5cd232e522adba866fa1ad7c36
Estiiven <122229140+estiivenCa@users.noreply.github.com>
2024-10-07 08:26:15 -0500

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;Stiiven⁩;;\nFN:Stiiven⁩\nORG:Stiiven⁩\nTITLE:\nitem1.TEL;waid=3204545069:3204545069\nitem1.X-ABLabel:Stiiven\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:KanBot-OFC⁩\nEND:VCARD`
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;Stiiven⁩;;\nFN:Stiiven⁩\nORG:Stiiven⁩\nTITLE:\nitem1.TEL;waid=573204545069:573204545069\nitem1.X-ABLabel:Stiiven\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:KanBot-OFC⁩\nEND:VCARD`
 await conn.sendMessage(m.chat, { contacts: { displayName: 'Kan.By.Stiiven', contacts: [{ vcard }] }}, {quoted: m})
 }
 handler.help = ['owner']
