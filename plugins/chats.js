4922c005a214739c021444d36df45c6e263eca41
Estiiven <122229140+estiivenCa@users.noreply.github.com>
2024-10-07 20:29:41 -0500

    Create chats.js

---
+++ plugins/chats.js
@@ -0,0 +1,27 @@
+import { totalmem, freemem } from 'os'
+import os from 'os'
+import util from 'util'
+import osu from 'node-os-utils'
+import { performance } from 'perf_hooks'
+import { sizeFormatter } from 'human-readable'
+import speed from 'performance-now'
+import { spawn, exec, execSync } from 'child_process'
+const format = sizeFormatter({ std: 'JEDEC', decimalPlaces: 2, keepTrailingZeroes: false, render: (literal, symbol) => `${literal} ${symbol}B` })
+
+var handler = async (m, { conn }) => {
+let chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
+let texto = ` _*Chats Totales:*_ ${chats.length}`.trim()
+
+m.react('⚠️')
+
+conn.reply(m.chat, texto, m, rcanal, )
+
+}
+handler.command = ['chats']
+export default handler
+
+function clockString(ms) {
+let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')} 
