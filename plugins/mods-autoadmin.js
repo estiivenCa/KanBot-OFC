6742330837b59ac50ea87f64d01e8e3b2bfd3eb5
Estiiven <122229140+estiivenCa@users.noreply.github.com>
2024-10-08 15:25:21 -0500

    Update mods-autoadmin.js

--- plugins/mods-autoadmin.js
+++ plugins/mods-autoadmin.js
@@ -10,7 +10,7 @@ const handler = async (m, {conn, isAdmin, groupMetadata }) => {
     m.reply('🚩 Ocurrio un error.');
   }
 };
-handler.tags = ['mods'];
+handler.tags = ['owner'];
 handler.help = ['autoadmin'];
 handler.command = ['autoadmin'];
 handler.rowner = true;
