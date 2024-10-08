2f2ecc98737f8a29cda27edddaa5488b7ae2188a
Estiiven <122229140+estiivenCa@users.noreply.github.com>
2024-10-08 15:27:47 -0500

    Update mods-unbanuser.js

--- plugins/mods-unbanuser.js
+++ plugins/mods-unbanuser.js
@@ -22,7 +22,7 @@ const handler = async (m, { conn, args, text, usedPrefix, command }) => {
 };
 handler.help = ['unbanuser <@tag>'];
 handler.command = ['unbanuser'];
-handler.tags = ['mods'];
+handler.tags = ['owner'];
 handler.mods = true;
 handler.group = true;
 export default handler;
